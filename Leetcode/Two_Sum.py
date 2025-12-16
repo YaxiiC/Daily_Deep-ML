c8a112cec3ca3b1278407e120f93d252bbd08328
YaxiiC <yaxi.chen.20@ucl.ac.uk>
2025-12-16 22:37:59 +0000

    20251216

--- Leetcode/Two_Sum.py
+++ Leetcode/Two_Sum.py
@@ -63,3 +63,30 @@ class Solution:
                 closest_number = nums[i]
 
         return closest_number
+
+'''
+No. 1768 Merge Strings Alternately
+
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+'''
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merged_string = []
+        len1 = len(word1)
+        len2 = len(word2)
+        min_len = min(len1, len2)
+
+        for i in range(min_len):
+            merged_string.append(word1[i])
+            merged_string.append(word2[i])
+
+        if len1 > len2:
+            merged_string.extend(word1[min_len:])
+        else:
+            merged_string.extend(word2[min_len:])
+
+        return "".join(merged_string)
+
+print(Solution().mergeAlternately("abc", "pqr"))
